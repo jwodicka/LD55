92fb19e964f88b127b8fb96b0e9c0514fe349683
Jason Wodicka <jason@wodicka.net>
2024-04-13 15:13:30 -0700

    Added a main menu

--- GameLogic.gd
+++ GameLogic.gd
@@ -1,4 +1,28 @@
 class_name GameLogic
 
+const SUMMONING_CIRCLE_SCENE = preload("res://summoning_circle.tscn")
+
 enum Symbol {EARTH, AIR, FIRE, WATER, SALT}
 enum State {INCOMPLETE, ERROR, CORRECT}
+
+static func load_level(level_name: String) -> SummoningCircle:
+	
+	var glyphs : Array[Symbol] = []
+	var targets : int = 1
+	var target_connectors : Array[Vector2i] = []
+	var offset_angle : float = 0
+	match level_name:
+		"Level 1":
+			glyphs = [Symbol.FIRE]
+		"Level 2":
+			glyphs = [Symbol.FIRE, Symbol.WATER, Symbol.FIRE]
+			targets = 2
+			target_connectors = [Vector2i(0, 1)]
+			offset_angle = -90
+	var level = SUMMONING_CIRCLE_SCENE.instantiate() as SummoningCircle
+	level.glyphs = glyphs
+	level.targets = targets
+	level.target_connectors = target_connectors
+	level.offset_angle = offset_angle
+	return level
+		
---
+++ MainMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+const LEVELS: Dictionary = {}
+
+func _on_level_select(level_name: String) -> void:
+	var level = GameLogic.load_level(level_name)
+	get_tree().root.add_child(level)
+	get_tree().root.remove_child(self)
+	self.queue_free
